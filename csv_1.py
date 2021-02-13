835a48cf12c1a073e63e76d38b51f881526c2cc3
alina <a.sarzhanova@g.nsu.ru>
2021-02-13 14:34:08 +0700

    Сохраняет все объявления, которые имеют более трёх изображений

---
+++ csv_1.py
@@ -0,0 +1,11 @@
+import csv
+with open('ex.csv', 'w', encoding="utf8") as new_csvfile:
+    with open('stage3_test.csv', encoding="utf8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        writer = csv.DictWriter(new_csvfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        for row in reader:
+            if row['Images'].count(',') > 2:
+                writer.writerow(row)
+
+
