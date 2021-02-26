db3d1cb0d0396bc89b9cdad9f444c87ee56d2df3
alina <a.sarzhanova@g.nsu.ru>
2021-02-26 18:49:43 +0700

    2 задание: Список персонажей пьесс (обновленный)

--- json_2.py
+++ json_2.py
@@ -1,6 +1,5 @@
 import json
 
-new_dict_obj = {}
 characters = []
 with open('json_new.json', 'w', encoding='utf-8') as new_file:
     with open('RomeoAndJuliet.json', 'r', encoding='utf-8') as file:
@@ -9,7 +8,7 @@ with open('json_new.json', 'w', encoding='utf-8') as new_file:
             for scenes in acts["scenes"]:
                 for action in scenes["action"]:
                     if action['character'] not in characters:
-                        characters.append(action['character'])#
+                        characters.append(action['character'])
                 new_json = json.dumps(characters)
                 new_file.write(new_json + '\n')
                 characters = []
