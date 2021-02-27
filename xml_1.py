20a4b20fab64cc2ce41438790c9debe00da37a3b
alina <a.sarzhanova@g.nsu.ru>
2021-02-27 17:28:43 +0700

    1 задание: Все предложения корпуса

--- xml_1.py
+++ xml_1.py
@@ -3,11 +3,8 @@ import xml.etree.ElementTree as etree
 tree = etree.parse('annot.opcorpora.no_ambig.xml')
 root = tree.getroot()
 with open('new.xml', 'w', encoding='utf-8') as file:
-    for text in root.findall('text'):
-        for paragraphs in text.findall('paragraphs'):
-            for paragraph in paragraphs.findall('paragraph'):
-                for sentence in paragraph.findall('sentence'):
-                    source = sentence.find('source').text
-                    file.write(source + '\n')
+    for source in root.iter('source'):
+        file.write(source.text + '\n')
+print('Done!')
 
 
