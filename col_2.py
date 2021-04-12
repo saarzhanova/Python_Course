7cf83c7ceace5193329d72cca19dd1b1b2c6bb5f
alina <a.sarzhanova@g.nsu.ru>
2021-04-12 12:11:08 +0700

    Collections 2 Задание (обновленное)

--- col_2.py
+++ col_2.py
@@ -1,15 +1,11 @@
 from collections import defaultdict
 import json
 
-b = []
 d = defaultdict(list)
 with open('RomeoAndJuliet.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
     for acts in data['acts']:
         for scenes in acts['scenes']:
             for action in scenes['action']:
-                a = (action['character'], action['says'])
-                b.append(a)
-for k, v in b:
-    d[k].append(v)
-print(d.items())
+                d[action['character']].append(action['says'])
+print(d)
