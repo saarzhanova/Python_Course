e0138e36c7c1057dad792f3d60edc3035d94be25
alina <a.sarzhanova@g.nsu.ru>
2021-04-12 12:50:49 +0700

    Time 3 Задание (обновленное)

--- time_3.py
+++ time_3.py
@@ -2,5 +2,5 @@ import time
 
 x = input('Введите дату рождения (ДД/ММ/ГГГГ): ')
 then = time.mktime(time.strptime(x,'%d/%m/%Y'))
-now = time.mktime(time.localtime())
+now = time.time()
 print("Поздравляю! Вы прожили " + str((now-then)//86400) + " дней!")
